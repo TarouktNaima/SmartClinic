7845b8a0d1f2c1d1a7fe26e0b517faf159659a37
Naima <tarouktnaima@gmail.com>
2026-05-05 16:33:10 +0100

    ADD

--- Api/app/Http/Controllers/DoctorController.php
+++ Api/app/Http/Controllers/DoctorController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Doctor;
-
+use Illuminate\Http\Request;
 class DoctorController extends Controller
 {
     public function index()
@@ -12,4 +12,32 @@ class DoctorController extends Controller
 
         return response()->json($doctors);
     }
+   public function store(Request $request)
+{
+    $request->validate([
+        'name' => 'required',
+        'specialite' => 'required',
+        'photo' => 'nullable|image|mimes:jpg,jpeg,png|max:2048',
+    ]);
+
+    $photoName = null;
+
+    if ($request->hasFile('photo')) {
+        $photo = $request->file('photo');
+        $photoName = time() . '.' . $photo->getClientOriginalExtension();
+
+        $photo->storeAs('doctors', $photoName, 'public');
+    }
+
+    $doctor = \App\Models\Doctor::create([
+        'name' => $request->name,
+        'specialite' => $request->specialite,
+        'photo' => $photoName,
+    ]);
+
+    return response()->json([
+        'message' => 'Doctor added successfully',
+        'doctor' => $doctor
+    ]);
+}
 }
